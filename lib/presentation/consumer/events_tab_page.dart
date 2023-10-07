c21011e892158706303412043fef4d8fb02fc1f3
sandeeprahul <sandeeprahul156@gmail.com>
2023-10-07 23:10:32 +0530

    web download option

--- lib/presentation/consumer/events_tab_page.dart
+++ lib/presentation/consumer/events_tab_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:photostatus/presentation/PhoneVerificationPage.dart';
 
 import 'book_event_page.dart';
 
